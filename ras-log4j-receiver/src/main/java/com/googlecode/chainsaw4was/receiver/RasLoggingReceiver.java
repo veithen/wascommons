f9acb55a0d96e8b67ce7dfc1b31af35f1b327ec3
Andreas Veithen <andreas.veithen@gmail.com>
2010-08-09 17:39:52 +0000

    Changed groupId and package name from com.google.code.chainsaw4was to com.googlecode.chainsaw4was.

--- ras-log4j-receiver/src/main/java/com/googlecode/chainsaw4was/receiver/RasLoggingReceiver.java
+++ ras-log4j-receiver/src/main/java/com/googlecode/chainsaw4was/receiver/RasLoggingReceiver.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.code.chainsaw4was.receiver;
+package com.googlecode.chainsaw4was.receiver;
 
 import java.io.File;
 import java.lang.reflect.Field;
