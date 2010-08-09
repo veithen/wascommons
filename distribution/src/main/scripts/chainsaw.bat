85efccd05f6177ce170b62612a8bebb83e9fb267
Andreas Veithen <andreas.veithen@gmail.com>
2010-08-09 08:44:23 +0000

    Initial distribution.

---
+++ distribution/src/main/scripts/chainsaw.bat
@@ -0,0 +1,14 @@
+@echo off
+set LIBDIR=%~dp0lib
+set CHAINSAW_CLASSPATH=conf
+setlocal EnableDelayedExpansion
+for %%c in ("%LIBDIR%\*.jar") do set CHAINSAW_CLASSPATH=!CHAINSAW_CLASSPATH!;%%c
+if "%JAVA_HOME%" == "" goto noJavaHome
+set JAVACMD=%JAVA_HOME%\bin\java.exe
+goto runChainsaw
+
+:noJavaHome
+set JAVACMD=java.exe
+
+:runChainsaw
+"%JAVACMD%" -cp "%CHAINSAW_CLASSPATH%" org.apache.log4j.chainsaw.LogUI
